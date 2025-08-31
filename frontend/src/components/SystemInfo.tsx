9db34a9a2982ff5f798f16a3911a9edfbf554430
Karol <71352077+hattimon@users.noreply.github.com>
2025-09-01 00:58:11 +0200

    Update SystemInfo.tsx

--- frontend/src/components/SystemInfo.tsx
+++ frontend/src/components/SystemInfo.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Sidebar } from './Sidebar';
+import { getSystem } from '../services/api';
 
 export const SystemInfo: React.FC = () => {
-  return <div className="p-4">Informacje systemowe - SUPER DOCKER PANEL</div>;
+  const [system, setSystem] = useState({});
+  const [token, setToken] = useState(localStorage.getItem('token') || '');
+
+  useEffect(() => {
+    if (token) {
+      getSystem(token).then(data => setSystem(data));
+    }
+  }, [token]);
+
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="p-4">
+        <h2>Informacje systemowe</h2>
+        <pre>{JSON.stringify(system, null, 2)}</pre>
+      </div>
+    </div>
+  );
 };
