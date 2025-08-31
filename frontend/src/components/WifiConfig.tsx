95e18ba18adee4e583c29674ba413a6bd6971156
Karol <71352077+hattimon@users.noreply.github.com>
2025-09-01 00:57:52 +0200

    Update WifiConfig.tsx

--- frontend/src/components/WifiConfig.tsx
+++ frontend/src/components/WifiConfig.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Sidebar } from './Sidebar';
 
 export const WifiConfig: React.FC = () => {
-  return <div className="p-4">Konfiguracja WiFi - SUPER DOCKER PANEL</div>;
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="p-4">Konfiguracja WiFi - W budowie (użyj nmcli w backend).</div>
+    </div>
+  );
 };
