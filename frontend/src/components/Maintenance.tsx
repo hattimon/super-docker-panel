3016c0c8b72e3a7c9c12a491dab4a3214a72be03
Karol <71352077+hattimon@users.noreply.github.com>
2025-09-01 00:58:27 +0200

    Update Maintenance.tsx

--- frontend/src/components/Maintenance.tsx
+++ frontend/src/components/Maintenance.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Sidebar } from './Sidebar';
 
 export const Maintenance: React.FC = () => {
-  return <div className="p-4">Maintenance - SUPER DOCKER PANEL</div>;
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="p-4">Maintenance - W budowie (użyj docker system prune).</div>
+    </div>
+  );
 };
