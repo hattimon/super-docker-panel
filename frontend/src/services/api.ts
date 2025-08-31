07a0fd49a09a7e6289dc160a28995168f25ed59d
Karol <71352077+hattimon@users.noreply.github.com>
2025-08-31 22:34:00 +0200

    Create api.ts

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api';
+
+export const login = async (username: string, password: string) => {
+  const response = await axios.post(`${API_URL}/login`, { username, password });
+  return response.data;
+};
+
+export const getContainers = async (token: string) => {
+  const response = await axios.get(`${API_URL}/containers`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data;
+};
